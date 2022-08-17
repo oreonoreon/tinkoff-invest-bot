2af57a31400dc74b1233f361af268e2116ae7508
oreonoreon <91880532+oreonoreon@users.noreply.github.com>
2022-08-17 20:05:37 +0300

    csvReadWrite.go служит для чтение скаченых у Тинькофф cvs файлов с минутными свечами инерполированием пропущеных свечей(тех свечей в которых не было торгов) и далее записи всех свечей в один файл. Далее есть возможность выделение и записи 15 мин. свечей и не только в отдельный файл.

--- internal/ema/csvReadWrite.go
+++ internal/ema/csvReadWrite.go
@@ -19,85 +19,43 @@ type Candle struct {
 	LowPrice   float64
 	VolumeLot  int64
 
-	Ema10_1min  float64 `csv:",omitempty"`
-	Ema10_5min  float64 `csv:"-"`
-	Ema10_15min float64 `csv:"-"`
-	Ema10_30min float64 `csv:"-"`
-	Ema10_1h    float64 `csv:"-"`
-
-	Ema20_1min  float64 `csv:"-"`
-	Ema20_5min  float64 `csv:"-"`
-	Ema20_15min float64 `csv:"-"`
-	Ema20_30min float64 `csv:"-"`
-	Ema20_1h    float64 `csv:"-"`
-
-	Ema50_1min  float64 `csv:"-"`
-	Ema50_5min  float64 `csv:"-"`
-	Ema50_15min float64 `csv:"-"`
-	Ema50_30min float64 `csv:"-"`
-	Ema50_1h    float64 `csv:"-"`
+	Ema10_1min float64 `csv:",omitempty"`
+	//Ema10_5min  float64 `csv:"-"`
+	//Ema10_15min float64 `csv:"-"`
+	//Ema10_30min float64 `csv:"-"`
+	//Ema10_1h    float64 `csv:"-"`
+	//
+	//Ema20_1min  float64 `csv:"-"`
+	//Ema20_5min  float64 `csv:"-"`
+	//Ema20_15min float64 `csv:"-"`
+	//Ema20_30min float64 `csv:"-"`
+	//Ema20_1h    float64 `csv:"-"`
+	//
+	//Ema50_1min  float64 `csv:"-"`
+	//Ema50_5min  float64 `csv:"-"`
+	//Ema50_15min float64 `csv:"-"`
+	//Ema50_30min float64 `csv:"-"`
+	//Ema50_1h    float64 `csv:"-"`
 }
 
 //`csv:",omitempty"`
 
 var Candles []Candle
 
-func ReadCSV(figi string) [][]string {
-	//file, err := os.Open(fmt.Sprint(figi, "/", figi, "_", "20220101", ".csv"))
-	dirEntry, err := os.ReadDir(figi)
-	if err != nil {
-		log.Fatalf("Error when opening file: %s", err)
-	}
-	filename := dirEntry[0].Name()
-	file, err := os.Open(figi + "/" + filename)
-	if err != nil {
-		log.Fatalf("Error when opening file: %s", err)
-	}
-	defer file.Close()
-	reader := csv.NewReader(file)
-	reader.Comma = ';'
-	rows, err := reader.ReadAll()
-	if err != nil {
-		log.Println("Cannot read CSV file:", err)
-	}
-	return rows
-	/*
-		fileScanner := bufio.NewScanner(file)
-		// read line by line
-		for fileScanner.Scan() {
-			fmt.Println(fileScanner.Text())
-		}
-		// handle first encountered error while reading
-		if err := fileScanner.Err(); err != nil {
-			log.Fatalf("Error while reading file: %s", err)
-		}
-	*/
-}
-
-/*
-func WriteInCandle(rows [][]string) {
-	for _, row := range rows {
-		//fmt.Println(row[0])
-		candle := Candle{
-			Name:       row[0],
-			Time:       row[1],
-			OpenPrice:  row[2],
-			ClosePrice: row[3],
-			MaxPrice:   row[4],
-			LowPrice:   row[5],
-			VolumeLot:  row[6],
-		}
-		Candles = append(Candles, candle)
-	}
-}
-*/
-
 func ReadWriteCSV(figi string) {
 	dirEntry, err := os.ReadDir(figi)
 	if err != nil {
 		log.Fatalf("Error when opening file: %s", err)
 	}
-	filename := dirEntry[0].Name()
+	for _, v := range dirEntry {
+		Candles = ReadCSV(v.Name(), figi)
+	}
+
+	WriteCSV(Candles)
+
+}
+
+func ReadCSV(filename string, figi string) []Candle {
 	file, err := os.Open(figi + "/" + filename)
 	if err != nil {
 		log.Fatalf("Error when opening file: %s", err)
@@ -124,19 +82,88 @@ func ReadWriteCSV(figi string) {
 		} else if err != nil {
 			log.Fatalln("error while dec.Decode ", err)
 		}
-		candle.countEma(10)
-		fmt.Println(candle)
-		Candles = append(Candles, candle)
+		if len(Candles) != 0 {
+			candle.interpolate()
+		} else {
+			Candles = append(Candles, candle)
+		}
+
 	}
-	WriteCSV(Candles)
-	//fmt.Printf("%+v", Candles)
+
+	for {
+		oldCandle := Candles[len(Candles)-1]
+		if (oldCandle.Time.Minute()+1)%30 == 0 {
+			break
+		} else {
+			////oldCandle.Time = oldCandle.Time.Add(time.Minute)
+			//oldCandle = Candle{
+			//	Name:       oldCandle.Name,
+			//	Time:       oldCandle.Time.Add(time.Minute),
+			//	OpenPrice:  oldCandle.ClosePrice,
+			//	ClosePrice: oldCandle.ClosePrice,
+			//	MaxPrice:   oldCandle.ClosePrice,
+			//	LowPrice:   oldCandle.ClosePrice,
+			//	VolumeLot:  0,
+			//}
+			////oldCandle.VolumeLot = 0
+			//Candles = append(Candles, oldCandle)
+			newInterpolateCandle(oldCandle)
+		}
+
+	}
+
+	return Candles
+}
+func newInterpolateCandle(oldCandle Candle) {
+
+	//oldCandle.Time = oldCandle.Time.Add(time.Minute)
+	oldCandle = Candle{
+		Name:       oldCandle.Name,
+		Time:       oldCandle.Time.Add(time.Minute),
+		OpenPrice:  oldCandle.ClosePrice,
+		ClosePrice: oldCandle.ClosePrice,
+		MaxPrice:   oldCandle.ClosePrice,
+		LowPrice:   oldCandle.ClosePrice,
+		VolumeLot:  0,
+	}
+	//oldCandle.VolumeLot = 0
+	Candles = append(Candles, oldCandle)
+}
+func (candle Candle) interpolate() {
+	oldCandle := Candles[len(Candles)-1]
+	dur := candle.Time.Sub(oldCandle.Time)
+	if !(dur == time.Minute || dur >= time.Hour) {
+		var i float64
+		for i = 1; i < dur.Minutes(); i++ {
+
+			//oldCandle = Candle{
+			//	Name:       oldCandle.Name,
+			//	Time:       oldCandle.Time.Add(time.Minute),
+			//	OpenPrice:  oldCandle.ClosePrice,
+			//	ClosePrice: oldCandle.ClosePrice,
+			//	MaxPrice:   oldCandle.ClosePrice,
+			//	LowPrice:   oldCandle.ClosePrice,
+			//	VolumeLot:  0,
+			//}
+			////oldCandle.VolumeLot = 0
+			//Candles = append(Candles, oldCandle)
+			newInterpolateCandle(oldCandle)
+			oldCandle.Time = oldCandle.Time.Add(time.Minute)
+		}
+	}
+	Candles = append(Candles, candle)
 }
 
-func (candle *Candle) countEma(averageCoefficient float64) {
+func CountEma(Candles []Candle, averageCoefficient float64) {
 	a := 2 / (averageCoefficient + 1)
-	price := candle.ClosePrice //цена закрытия
-
-	candle.Ema10_1min = a*price + (1-a)*candle.Ema10_1min
+	for k, v := range Candles {
+		if k != 0 {
+			v.Ema10_1min = a*v.ClosePrice + (1-a)*Candles[k-1].Ema10_1min
+		} else {
+			v.Ema10_1min = v.ClosePrice
+		}
+		Candles[k] = v
+	}
 }
 
 func WriteCSV(Candles []Candle) {
@@ -158,4 +185,88 @@ func WriteCSV(Candles []Candle) {
 		fmt.Println("error:", err)
 	}
 
+}
+
+func NewTimeFrame(n int) {
+
+	file, err := os.Create(fmt.Sprint("1_", n, ".csv"))
+	if err != nil {
+		log.Fatalf("Error when create file: %s", err)
+	}
+	defer file.Close()
+	//
+	w := csv.NewWriter(file)
+	w.Comma = ';'
+	for k, v := range Candles {
+		if v.Time.Minute()%n == 0 {
+			//smax,smin,vol:=MAXLOWpriceANDVolume(Candles[k : k+n])
+			newCandle := Candle{
+				Name:       v.Name,
+				Time:       v.Time,
+				OpenPrice:  v.OpenPrice,
+				ClosePrice: Candles[k+n-1].ClosePrice,
+				MaxPrice:   MaxPriceCount(Candles[k : k+n]),
+				LowPrice:   LowPriceCount(Candles[k : k+n]),
+				VolumeLot:  VolumeLotCount(Candles[k : k+n]),
+			}
+			enc := csvutil.NewEncoder(w)
+			enc.AutoHeader = false
+			if err = enc.Encode(newCandle); err != nil {
+				fmt.Println("error:", err)
+			}
+		}
+	}
+	//w := csv.NewWriter(file)
+	//w.Comma = ';'
+	//if err = csvutil.NewEncoder(w).Encode(Candles); err != nil {
+	//	fmt.Println("error:", err)
+	//}
+
+	w.Flush()
+	if err = w.Error(); err != nil {
+		fmt.Println("error:", err)
+	}
+}
+
+func VolumeLotCount(candles []Candle) int64 {
+	var s int64
+	for _, v := range candles {
+		s += v.VolumeLot
+	}
+	return s
+}
+func LowPriceCount(candles []Candle) float64 {
+	var s float64 = candles[0].LowPrice
+	for _, v := range candles {
+		if v.LowPrice < s {
+			s = v.LowPrice
+		}
+	}
+	return s
+}
+func MaxPriceCount(candles []Candle) float64 {
+	var s float64
+	for _, v := range candles {
+		if v.MaxPrice > s {
+			s = v.MaxPrice
+		}
+	}
+
+	return s
+}
+
+func MAXLOWpriceANDVolume(candles []Candle) (float64, float64, int64) {
+	var smax, smin float64
+	var vol int64
+	smin = candles[0].LowPrice
+	for _, v := range candles {
+		if v.MaxPrice > smax {
+			smax = v.MaxPrice
+		}
+		if v.LowPrice < smin {
+			smin = v.LowPrice
+		}
+		vol += v.VolumeLot
+	}
+	return smax, smin, vol
 }
